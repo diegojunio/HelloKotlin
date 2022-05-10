835f9abb6ee2a529e2ab7ef91606aaf2b0dfb06e
diegojunio <diegojunio@gmail.com>
2022-05-10 17:50:27 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+import java.util.*
+
+fun main(Args: Array<String>){
+    feedTheFish()
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and the fish eat $food.")
+}
+
+fun randomDay() : String {
+    val week = arrayOf(
+        "Monday", "Tuesday",
+        "Wednesday", "Thursday",
+        "Friday","Saturday",
+        "Sunday")
+
+    return week[Random().nextInt(week.size)]
+}
+
+fun fishFood (day: String) : String  {
+    var food = ""
+    when (day) {
+        "Monday" -> food = "flakes"
+        "Tuesday" -> food = "pellets"
+        "Wednesday" -> food = "redworms"
+        "Thursday" -> food = "granules"
+        "Friday" -> food = "mosquitoes"
+        "Saturday" -> food = "lettuce"
+        "Sunday" -> food = "plankton"
+    }
+    return food
+}
